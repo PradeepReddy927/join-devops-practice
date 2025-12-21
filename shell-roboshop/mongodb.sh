ff7c10ee6b1dbe304623793b5700c10359dc30cc
pradeepreddy25 <prem.pradeepreddy19@gmail.com>
2025-12-21 18:19:12 +0530

    practice-session

--- shell-roboshop/mongodb.sh
+++ shell-roboshop/mongodb.sh
@@ -12,7 +12,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 
-Mkdir -p $LOGS_FOLDER
+mkdir -p $LOGS_FOLDER
 echo "Script started executed at:$(date)" | tee -a $LOG_FILE
 
 if [$USERID -ne 0]; then
